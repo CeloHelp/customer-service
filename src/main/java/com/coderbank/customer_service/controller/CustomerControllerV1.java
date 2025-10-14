6dc9cb468ca5b324f755696b52ab6bbe0be8cb4d
Marcelo <marcelopacobello@gmail.com>
2025-10-14 20:18:50 -0300

    feat: criação da camada de controller + primeiro end-point

---
+++ src/main/java/com/coderbank/customer_service/controller/CustomerControllerV1.java
@@ -0,0 +1,47 @@
+package com.coderbank.customer_service.controller;
+
+
+import com.coderbank.customer_service.dto.request.CustomerRequestDTO;
+import com.coderbank.customer_service.dto.response.CustomerResponseDTO;
+import com.coderbank.customer_service.mapper.CustomerMapper;
+import com.coderbank.customer_service.service.CustomerService;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("/api/v1/customers")
+public class CustomerControllerV1 {
+
+    private final CustomerService customerService;
+
+
+
+
+    public CustomerControllerV1(CustomerService customerService) {
+        this.customerService = customerService;
+
+    }
+
+
+    @PostMapping
+    public ResponseEntity<CustomerResponseDTO> createCustomer(@Valid @RequestBody CustomerRequestDTO customerRequestDTO) {
+
+        CustomerResponseDTO createdCustomer = customerService.createCustomer(customerRequestDTO);
+
+        URI location = URI.create(String.format("/api/v1/customers/%s", createdCustomer.id()));
+        // Retorna 201 Created com o local do novo recurso no cabeçalho Location
+
+        return ResponseEntity.status(201).body(createdCustomer);
+
+
+    }
+
+
+
+}
